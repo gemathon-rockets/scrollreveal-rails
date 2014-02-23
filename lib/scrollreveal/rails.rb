a1feef4394af844c852a38b31d883206b74dfa95
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-02-23 18:04:10 -0500

    Precompile scrollreveal.js

--- lib/scrollreveal/rails.rb
+++ lib/scrollreveal/rails.rb
@@ -1,6 +1,9 @@
 module Scrollreveal
   module Rails
     class Engine < ::Rails::Engine
+    	initializer "precompile", :group => :all do |app|
+       	  app.config.assets.precompile << Proc.new{|path| path == "scrollReveal.js" }
+        end
     end
   end
 end
